f52ec3d6aabf4db129d1bc0e10e8479f3d62c295
WANG <nayu441@163.com>
2024-04-19 21:41:57 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -96,4 +96,15 @@ public class EmployeeController {
 
         return Result.success(pageResult);
     }
+
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("启用禁用员工账号,状态：{},id:{}",status,id);
+
+        employeeService.startOrStop(status,id);
+
+        return Result.success();
+    }
 }
