ce55374f2f5ce6580df722367bbadad10c1a18f8
WANG <nayu441@163.com>
2024-04-22 18:46:23 +0800

    新增分类业务代码开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -28,9 +28,23 @@ public interface EmployeeService {
      */
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
     void startOrStop(Integer status, Long id);
 
+    /**
+     * 根据id查询员工数据
+     * @param id
+     * @return
+     */
     Employee selectByEmpId(Integer id);
 
+    /**
+     * 编辑员工数据
+     * @param employeeDTO
+     */
     void updateEmp(EmployeeDTO employeeDTO);
 }
