51a1089dd36c41ca0cde0f383768c5f308b563bc
WANG <nayu441@163.com>
2024-06-02 21:29:24 +0800

    删除菜品代码优化

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -97,14 +97,21 @@ public class DishServiceImpl implements DishService {
         }
 
         //删除菜品表中的菜品数据
+        /*
         for (Long id : ids) {
             dishMapper.deleteById(id);
             //删除菜品关联的口味数据
             dishFlavorMapper.deleteByDishId(id);
         }
+        */
 
-
-
+        //优化代码 减少发送的sql语句数量
+        //根据id批量删除菜品数据
+        //sql：delete from dish where id in (?,?,?)
+        dishMapper.deleteByIds(ids);
+        //根据id批量删除菜品关联的口味数据
+        //sql：delete from dish_flavor where dish_id in (?,?,?)
+        dishFlavorMapper.deleteByDishIds(ids);
 
     }
 }
