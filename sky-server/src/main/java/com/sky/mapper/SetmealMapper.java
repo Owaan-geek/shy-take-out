ab20cfb4d7554870906498eba4594387bc750c4e
WANG <nayu441@163.com>
2024-06-08 17:50:51 +0800

    新增套餐代码实现

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,5 +1,6 @@
 package com.sky.mapper;
 
+import com.sky.entity.Setmeal;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -13,4 +14,16 @@ public interface SetmealMapper {
      */
     @Select("select count(id) from sky_take_out.setmeal where category_id = #{categoryId}")
     Integer countByCategoryId(Long id);
+
+    /**
+     * 根据id修改套餐
+     * @param setmeal
+     */
+    void update(Setmeal setmeal);
+
+    /**
+     * 新增套餐
+     * @param setmeal
+     */
+    void insert(Setmeal setmeal);
 }
